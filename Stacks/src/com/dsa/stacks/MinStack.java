86609963ab39629cb47fb3f0ecc45c1f0a8b40ab
saimeda <saimedaiiitg19@gmail.com>
2025-07-16 23:35:13 +0530

    Min Stack.

---
+++ Stacks/src/com/dsa/stacks/MinStack.java
@@ -0,0 +1,49 @@
+package com.dsa.stack;
+
+import java.util.Stack;
+
+public class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        // Only push to minStack if it's empty or val is new minimum
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) return;
+        int val = stack.pop();
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println("Min: " + minStack.getMin()); // -3
+        minStack.pop();
+        System.out.println("Top: " + minStack.top());    // 0
+        System.out.println("Min: " + minStack.getMin()); // -2
+    }
+}
